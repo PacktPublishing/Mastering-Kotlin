644c13b8fdedafb0f22cfd71a849f5d94cebd2d2
Nate Ebel <n8ebel@gmail.com>
2019-08-22 18:41:42 -0700

    Add initial js example

--- ch15/core/src/commonMain/kotlin/Sample.kt
+++ ch15/core/src/commonMain/kotlin/Sample.kt
@@ -1,9 +1,3 @@
 expect object Platform {
     val name: String
 }
-
-fun hello(): String = "Hello from ${Platform.name}"
-
-fun main() {
-    println(hello())
-}
---
+++ ch15/webapp/src/main/kotlin/main.kt
@@ -0,0 +1,4 @@
+fun main() {
+    val outputMessage = "Hello Kotlin ${Platform.name}"
+    println(outputMessage)
+}
